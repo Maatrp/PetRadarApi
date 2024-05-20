498d6f7c3c1848f8e81050dc5ef18771566fd4f8
marta <maat.08.04.93@gmail.com>
2024-05-20 21:11:37 +0200

    create place image

---
+++ src/main/java/com/api/petradar/placeimages/UploadPlaceImageService.java
@@ -0,0 +1,13 @@
+package com.api.petradar.placeimages;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UploadImageService {
+
+    public String uploadImage(UploadedImage uploadedImage) {
+        String url = "url";
+
+        return url;
+    }
+}
